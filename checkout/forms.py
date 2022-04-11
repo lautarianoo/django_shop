ffaef81f1eacadd8609d82666fcd01fd07eca00a
lautariano <lautariano777@gmail.com>
2022-04-11 21:00:23 +0500

    Begin make checkout forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from .models import ApplyOrganization
+
+class ApplyOrganizationForm(forms.ModelForm):
+
+    class Meta:
+        model = ApplyOrganization
+        exclude = ('accepted', 'no_accepted', )
+
+    def clean(self, *args , **kwargs):
+        data = self.cleaned_data
+        if ApplyOrganization.objects.filter(name_organization=data['name_organization']).exists():
+            raise forms.ValidationError("Заявка на эту организацию уже подана")
